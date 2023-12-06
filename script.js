b428e6c9a0569a57cbe0ff22323a292a70364511
~Erick <143459790+erickfesantos@users.noreply.github.com>
2023-12-06 18:59:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 let caixa = document.querySelector("div");
+let botao = document.querySelector("#botao");
 
-let promessa = new Promise((vdd, fal)=>{
-  setTimeout(()=>{
-    let destino = Math.random();
-    if(destino>0.5){
-      vdd("Deu tudo certo!");
-    }else{
-      fal("Deu tudo errado!");
-    }
-  },2000);
-});
+function funPromessa(){
+  let promessa = new Promise((vdd, fal)=>{
+    setTimeout(()=>{
+      let destino = Math.random();
+      if(destino>0.6){
+        vdd("Deu tudo certo!");
+      }else{
+        fal("Deu tudo errado!");
+      }
+    },1000);
+  });
 
-promessa.then((retorno)=>{
-  caixa.innerHTML=retorno;
-  caixa.classList.add("certo");
-});
+  promessa.then((retorno)=>{
+    caixa.innerHTML=retorno;
+    caixa.classList.remove("errado");
+    caixa.classList.add("certo");
+  });
 
-promessa.catch((retorno)=>{
-  caixa.innerHTML=retorno;
-  caixa.classList.add("errado");
-});
+  promessa.catch((retorno)=>{
+    caixa.innerHTML=retorno;
+     caixa.classList.remove("certo");
+    caixa.classList.add("errado");
+  });
+}
 
 
+botao.addEventListener("click", ()=>{
+  caixa.classList.remove("certo", "errado");
+  caixa.innerHTML="Processando...";
+  funPromessa();
+} );
 
-caixa.innerHTML="Processando...";
+
+caixa.innerHTML="Esperando...";
